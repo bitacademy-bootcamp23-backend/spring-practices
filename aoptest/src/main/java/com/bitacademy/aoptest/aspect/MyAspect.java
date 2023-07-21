b5b6386c264150eb2985e8c979665d5cad548717
BIT <BIT@DESKTOP-EJDVL85>
2023-07-21 14:25:41 +0900

    aoptest - finished

--- aoptest/src/main/java/com/bitacademy/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/bitacademy/aoptest/aspect/MyAspect.java
@@ -1,5 +1,10 @@
 package com.bitacademy.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,4 +17,38 @@ public class MyAspect {
 	public void adviceBefore() {
 		System.out.println("--- Before Advice ---");
 	}
+	
+	@After("execution(com.bitacademy.aoptest.vo.ProductVo com.bitacademy.aoptest.service.ProductService.find(String))")
+	public void adviceAfter() {
+		System.out.println("--- After Advice ---");
+	}
+
+	@AfterReturning("execution(* *..*.ProductService.find(String))")
+	public void adviceAfterReturning() {
+		System.out.println("--- AfterReturning Advice ---");
+	}
+
+	@AfterThrowing(value="execution(* *..*.ProductService.find(..))", throwing="ex")
+	public void adviceAfterThrowing(Throwable ex) {
+		System.out.println("--- AfterThrowing Advice :" + ex + "---");
+	}
+	
+	@Around("execution(* *..*.ProductService.*(String))")
+	public Object adviceAround(ProceedingJoinPoint pjp) throws Throwable {
+		/* before */
+		System.out.println("--- Around(Before) Advice ---");
+		
+		/* Point Cut Method 실행 */
+		
+		// Object[] parameters = {"Camera"};
+		// Object result = pjp.proceed(parameters);
+		
+		Object result = pjp.proceed();
+		
+		/* after */
+		System.out.println("--- Around(After) Advice ---");
+		
+		return result;
+	}
+	
 }
